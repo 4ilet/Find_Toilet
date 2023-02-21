dae38aef2700e5b7d7e6e859539ccdaa97f6d261
kaydenna92 <61959111+kaydennna92@users.noreply.github.com>
2023-02-21 15:41:50 +0900

    #21 [BE-장엽] feat: 환경설정

---
+++ BackEnd/src/test/java/com/ilet/ilet/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilet.ilet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
