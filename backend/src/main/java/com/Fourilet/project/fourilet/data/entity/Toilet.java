ee0bc33ff14bf6ec97aeefc812bd81c663add11c
gimewn <facquodvisy@naver>
2023-03-04 23:41:05 +0900

    #38 [BE-윤주] feat: 화장실 데이터 자료형 세부 수정

--- backend/src/main/java/com/Fourilet/project/fourilet/data/entity/Toilet.java
+++ backend/src/main/java/com/Fourilet/project/fourilet/data/entity/Toilet.java
@@ -27,28 +27,39 @@ public class Toilet {
 
     private String operationTime;
 
+    @Column(precision =18, scale = 10)
     private BigDecimal lat;
 
+    @Column(precision =18, scale = 10)
     private BigDecimal log;
 
     private String phoneNumber;
 
+    @Column(columnDefinition = "TINYINT(1)")
     private boolean dMalePee;
 
+    @Column(columnDefinition = "TINYINT(1)")
     private boolean dMalePoo;
 
+    @Column(columnDefinition = "TINYINT(1)")
     private boolean dFemalePee;
 
+    @Column(columnDefinition = "TINYINT(1)")
     private boolean dFemalePoo;
 
+    @Column(columnDefinition = "TINYINT(1)")
     private boolean cFemalePoo;
 
+    @Column(columnDefinition = "TINYINT(1)")
     private boolean cMalePee;
 
+    @Column(columnDefinition = "TINYINT(1)")
     private boolean cMalePoo;
 
+    @Column(columnDefinition = "TINYINT(1)")
     private boolean allDay;
 
+    @Column(columnDefinition = "TINYINT(1)")
     private boolean diaper;
 
 }
