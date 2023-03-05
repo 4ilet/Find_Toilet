d17f735524f3b8e3d346757f12e51773bf48902a
gimewn <facquodvisy@naver>
2023-03-05 15:00:22 +0900

    #38 [BE-윤주] feat: Security Config 설정

--- backend/src/main/java/com/Fourilet/project/fourilet/SecurityConfig.java
+++ backend/src/main/java/com/Fourilet/project/fourilet/SecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
