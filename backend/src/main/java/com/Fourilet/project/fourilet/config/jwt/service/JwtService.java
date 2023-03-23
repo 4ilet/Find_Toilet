92ba49a261d0dc9ecdd17f3c432ff0ea8b48b5b6
gimewn <yoonju_1120@naver.com>
2023-03-23 22:37:03 +0900

    #38 [BE-윤주] feat: token 유효성 체크 추가

--- backend/src/main/java/com/Fourilet/project/fourilet/config/jwt/service/JwtService.java
+++ backend/src/main/java/com/Fourilet/project/fourilet/config/jwt/service/JwtService.java
@@ -145,6 +145,21 @@ public class JwtService {
         }
     }
 
+    public Optional<Long> extractId(String accessToken) {
+        try {
+            // 토큰 유효성 검사하는 데에 사용할 알고리즘이 있는 JWT verifier builder 반환
+            return Optional.ofNullable(JWT.require(Algorithm.HMAC512(secretKey))
+                    .build() // 반환된 빌더로 JWT verifier 생성
+                    .verify(accessToken) // accessToken을 검증하고 유효하지 않다면 예외 발생
+                    .getClaim(ID_CLAIM) // claim(id) 가져오기
+                    .asLong());
+        } catch (Exception e) {
+            System.out.println("아이디 추출에서 문제 발생");
+            log.error("액세스 토큰이 유효하지 않습니다.");
+            return Optional.empty();
+        }
+    }
+
     /**
      * AccessToken 헤더 설정
      */
